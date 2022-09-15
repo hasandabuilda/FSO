cac6219795ba1339dab86376ad1918799700c0bb
hasandabuilda <hasandabuilda@proton.me>
2022-09-15 15:43:06 -0500

    Fetch data from server and use React Effects

--- part2-notes/src/index.js
+++ part2-notes/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
 
 const notes = [
   {
@@ -21,7 +22,5 @@ const notes = [
     important: true
   }
 ]
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+  
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
