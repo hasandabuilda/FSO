ba5177769b9162e9f6abceb2d7054b769a7c843c
hasandabuilda <hasandabuilda@proton.me>
2022-10-10 20:56:31 -0500

    Refactor to a more ideal express project structure

--- examples/ideal_express_structure/.eslintrc.js
+++ examples/ideal_express_structure/.eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
     'env': {
         'browser': true,
         'commonjs': true,
-        'es2021': true,
-        'jest': true,
+        'es2021': true
     },
     'extends': 'eslint:recommended',
     'overrides': [
