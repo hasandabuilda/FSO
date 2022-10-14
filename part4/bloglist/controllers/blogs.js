79a293303c74282dfbdeac7299d08d38adb2abbc
hasandabuilda <hasandabuilda@proton.me>
2022-10-14 11:51:31 -0500

    Test with Jest and use async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../model/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+    let blog
+    if (!('title' in request.body._doc) || !('url' in request.body._doc)) {
+        return response.status(400).end()
+    }
+    else if (!('likes' in request.body._doc)) {
+        blog = new Blog({...request.body, likes: 0})
+    }
+    else {
+        blog = new Blog(request.body)
+    }
+    const result = await blog.save()
+    response.status(201).json(result)
+})
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const blog = {
+        likes: request.body.likes
+    }
+
+    const newBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        blog,
+        { new: true }
+    )
+
+    response.json(newBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/listHelper.test.js
+++ part4/bloglist/tests/listHelper.test.js
@@ -1,55 +1,70 @@
 const listHelper = require('../utils/list_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../model/blog')
+
+const api = supertest(app)
 
 const masterBlogs = [
     {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
     },
     {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
     },
     {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
     },
     {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
+        _id: '5a422b891b54a676234d17fa',
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+        __v: 0
     },
     {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+        __v: 0
     },
     {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0
     }
 ]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = masterBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
 
 describe('total likes', () => {
     test('of empty list is zero', () => {
@@ -88,10 +103,10 @@ describe('favorite blogs', () => {
 
     test('gives most liked blog', () => {
         expect(listHelper.favoriteBlogs(masterBlogs)).toEqual({
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
             likes: 12,
             __v: 0
         })
@@ -113,5 +128,67 @@ describe('most likes', () => {
             author: 'Edsger W. Dijkstra',
             likes: 17
         })
+    })
+})
+
+describe('api calls', () => {
+    test('get', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(masterBlogs.length)
+    })
+
+    test('id', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
+
+    test('post', async () => {
+        const blog = new Blog({
+            title: 'Example',
+            author: 'Hasan',
+            url: 'www.example.com',
+            likes: 10
+        })
+
+        await api.post('/api/blogs').send(blog)
+
+        const blogsAtEnd = await api.get('/api/blogs')
+        expect(blogsAtEnd.body.length).toBe(masterBlogs.length + 1)
+    })
+
+    test('post without likes', async () => {
+        const blog = new Blog({
+            title: 'Example',
+            author: 'Hasan',
+            url: 'www.example.com'
+        })
+
+        await api.post('/api/blogs').send(blog)
+
+        const blogsAtEnd = await api.get('/api/blogs')
+        const lastBlog = blogsAtEnd.body[blogsAtEnd.body.length - 1]
+        expect(lastBlog.likes).toBe(0)
+    })
+
+    test('post without title or url return 400', async () => {
+        const blog = new Blog({
+            author: 'Hasan'
+        })
+
+        await api.post('/api/blogs/').send(blog).expect(400)
+    })
+
+    test('delete', async () => {
+        await api.delete('/api/blogs/5a422a851b54a676234d17f7')
+        const blogs = await api.get('/api/blogs')
+        expect(blogs.body.length).toBe(masterBlogs.length - 1)
+    })
+
+    test('put', async () => {
+        await api
+            .put('/api/blogs/5a422a851b54a676234d17f7')
+            .send({likes: 10})
+        const blogs = await api.get('/api/blogs')
+        expect(blogs.body[0].likes).toBe(10)
     })
 })
